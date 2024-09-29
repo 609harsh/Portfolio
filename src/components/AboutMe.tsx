8f4bc09cc812c38821344333bbd3ebd68db8a465
609harsh <harshgupta609@gmail.com>
2024-09-29 16:09:54 +0530

    profile image updated

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
       <h1 className="text-center font-bold text-3xl sticky top-0 bg-gray-50 z-10 p-4 font-prozo mt-6">About Me</h1>
       <div className="flex flex-col md:flex-row w-full">
         <div className="w-full md:w-2/5 p-7">
-          <img src="profile.png" className="rounded-[50%] "/>
+          <img src="profile.jpg" className="rounded-[50%] "/>
         </div>
         <div className=" w-full md:w-3/5 text-center font-baloo text-xl mx-auto md:my-auto">
           <p className=" font-baloo text-lg md:text-xl  h-full md:leading-loose p-3 md:p-6">
